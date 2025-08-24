307d6745354614ab2d0d8d68f030a1a1026dc971
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-08-24 12:36:19 -0400

    feat: agregar configuración de E2E en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  e2e: {
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+    }
+  },
   plugins: [react()],
 })
