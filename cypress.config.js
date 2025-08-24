9b2f6aed6c65e96c8980c8253453744137ace950
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-08-24 15:42:12 -0400

    fix: cypress-config.js

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,10 @@ export default defineConfig({
       // implement node event listeners here
     },
     baseUrl: "http://localhost:4173",
+    video: true,                     // activa la grabación de video
+    videosFolder: 'cypress/videos', // carpeta donde se guardan
+    videoCompression: 32,           // compresión (0 = sin comprimir)
+    videoUploadOnPasses: true,      // true: guarda siempre; false: solo si fallan
+    trashAssetsBeforeRuns: true     // borra videos anteriores antes de correr
   },
 });
