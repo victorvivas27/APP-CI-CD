a2fe6e7cab3e75b728234f89723f6dcfa6cbf69c
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-08-24 15:23:47 -0400

    fix: cypres.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "2n6w8u",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
