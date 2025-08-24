2b78bc2e88b8384b9a60ff8b7f1dcdb091062d54
Victor Javier Vivas <victorjaviervivas@gmail.com>
2025-08-24 15:38:17 -0400

    fix.:  pruebas-cypress.yml

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: "2n6w8u",
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents() {
       // implement node event listeners here
     },
     baseUrl: "http://localhost:4173",
